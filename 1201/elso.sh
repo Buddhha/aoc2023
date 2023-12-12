fff4d7ed14b4d8ce2db060826541f7b51f21172a
Buddhha <buddhha@gmail.com>
2023-12-12 17:08:55 +0100

    Add files via upload

--- 1201/elso.sh
+++ 1201/elso.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 filename='input.txt'
 calibration=()
+re='^[0-9]+$'
 
 while read line; do
 calibration[${#calibration[@]}]=$line
@@ -8,6 +9,14 @@ done < $filename
 
 for ((i = 0; i < ${#calibration[@]}; i++))
 do
-	echo ${calibration[i]}
+	for ((j = 0; j < ${#calibration[i]}; j++))
+	do
+		#echo -n ${calibration[i]:j:1}
+		if [[ ${calibration[i]:j:1} =~ $re ]] ; then
+		decimal=$((10*${calibration[i]:j:1}))
+		echo $decimal
+		fi
+	done
+	echo	
 done
 
